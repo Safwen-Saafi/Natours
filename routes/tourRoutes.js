5618af4f0c186c80100048f7767db8db97b30ce6
Safwen-Saafi <saafi.saf0@gmail.com>
2024-10-21 22:04:42 +0100

    Adding missing authentication and authorization

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,30 +1,49 @@
 const express = require('express');
-const {
-  getAllTours,
-  createTour,
-  getTour,
-  updateTour,
-  deleteTour,
-  aliasTopTours,
-  getTourStats,
-  getMonthlyPlan
-} = require('../controllers/tourController');
-const { protect } = require('./../controllers/authController');
-const reviewRouter = require('./reviewRoutes');
+const tourController = require('./../controllers/tourController');
+const authController = require('./../controllers/authController');
+const reviewRouter = require('./../routes/reviewRoutes');
 
 const router = express.Router();
-// router.param('id', tourController.checkID);
 
 // POST /tour/234fad4/reviews
 // GET /tour/234fad4/reviews
 
-router.use('/:tourId/reviews', reviewRouter); //Just to delete the confusion of having a review route inside a tour route
-router.route('/top-5-rated').get(aliasTopTours, getAllTours);
-router.route('/tours-stats').get(getTourStats);
-router.route('/').get(protect, getAllTours).post(createTour);
-router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
-router.route('/monthly-plan/:year').get(getMonthlyPlan);
+router.use('/:tourId/reviews', reviewRouter);
+
+router
+  .route('/top-5-rated')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
+router.route('/tour-stats').get(tourController.getTourStats);
+router
+  .route('/monthly-plan/:year')
+  .get(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide', 'guide'),
+    tourController.getMonthlyPlan
+  );
+
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.createTour
+  );
+
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.updateTour
+  )
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.deleteTour
+  );
 
 module.exports = router;
-
-// router.param('id', checkId); the function I used to check the id of the req.body but no longer need it
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,23 +6,28 @@ const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.get('/login', authController.login);
-
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
 
+
+// Protect all routes after this middleware
+router.use(authController.protect);
+
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
 router.patch(
   '/updateMyPassword',
-  authController.protect,
   authController.updatePassword
 );
 router.get(
   '/me',
-  authController.protect,
   userController.getMe,
   userController.getUser
 );
+
+router.use(authController.restrictTo('admin'));
+
+
 router
   .route('/')
   .get(userController.getAllUsers)
